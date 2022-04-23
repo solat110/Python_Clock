5365d6be7780c1b3a4d6c96ef1144a588a64f141
solat110 <solat.rizvi@gmail.com>
2022-04-24 04:51:34 +0500

    Add files via upload

---
+++ time_clock.py
@@ -0,0 +1,23 @@
+from tkinter import *
+from tkinter.ttk import *
+##import tkinter as Tk
+##import tkinter.ttk
+#import time
+#from tkinter import
+#from tkinter.ttk import
+
+from time import strftime
+
+root = Tk()
+root.title("Clock made by Solat")
+
+def time():
+	string = strftime('%H:%M:%S %p')
+	label.config(text=string)
+	label.after(1000, time)
+
+label = Label(root, font=("ds-digital" , 80), background = "cyan" , foreground = "green")
+label.pack(anchor="center")
+time()
+
+mainloop()
